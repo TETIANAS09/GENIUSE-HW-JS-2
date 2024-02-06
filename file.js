17673005f680d9c04e48f5fd101c1f46d6b3e3ef
TETIANAS09 <150555108+TETIANAS09@users.noreply.github.com>
2024-02-06 18:24:22 +0100

    Add files via upload

---
+++ file.js
@@ -0,0 +1,97 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+
+//Result//
+
+// варіант   1 //
+//console.log(1 > 0);
+//console.log(1 < 0);
+
+// варіант   0 //
+//console.log(0 >= 0);
+//console.log(0 < 0);
+
+// варіант -3 //
+//console.log(!-3 < 0);
+//console.log(-3 > 0);
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',
+//"qwerty", true
+
+//Result//
+
+/*
+const test = "test";
+if (test == "test");
+console.log("true");
+*/
+/*
+const qwerty = "test";
+if (qwerty !== "test");
+console.log("true");
+*/
+/*
+const test = true;
+if (test === true);
+console.log("true");
+*/
+
+//Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+
+//Result//
+
+/*
+console.log(1 + 5);
+console.log(10 - 5);
+console.log(13 - 5);
+*/
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+//Result//
+
+/*
+const year = prompt("enter  the number");
+if (year == "1") {
+  console.log("January");
+} else if (year == "2") {
+  console.log("February");
+} else if (year == "3") {
+  console.log("March");
+} else if (year == "4") {
+  console.log("April");
+} else if (year == "5") {
+  console.log("May");
+} else if (year == "6") {
+  console.log("June");
+} else if (year == "7") {
+  console.log("July");
+} else if (year == "8") {
+  console.log("August");
+} else if (year == "9") {
+  console.log("September");
+} else if (year == "10") {
+  console.log("October");
+} else if (year == "11") {
+  console.log("November");
+} else if (year == "12") {
+  console.log("December");
+}
+
+*/
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+//Result//
+
+/*
+let a = +prompt(" Ведіть  будь ласка Перше число !");
+let b = +prompt(" Ведіть  будь ласка  Друге число !");
+let c = +prompt(" Ведіть  будь ласка  Третє число!");
+alert(a + b + c);
+*/
